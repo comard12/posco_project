736e689961a9cb0b2d3f40a529dcb8a33a7431d2
sangwon <psw7528@gamil.com>
2023-09-18 13:23:36 +0900

    magic_square_ubdate_IsItOdd_Arraymk_Magicmk

--- cpp_posco_project/Project1/project4_magic_square.cpp
+++ cpp_posco_project/Project1/project4_magic_square.cpp
@@ -7,9 +7,45 @@ using namespace std;
 
 int N_size(int num_of_square); // 몇 자리인지 알려줌 (결과 출력 시 필요)
 void Say(int** array, int n); // 결과출력
+bool is_it_odd(int n); // 홀수인지 판별
+int** arraymk(int** array, int n); // 배열 생성
+int** magicmk(int** array, int n); // 마방진 생성
 
 
+int main() {
+	int n = 0;
+	int num_of_square = 0;
 
+	cout << "N의 값(홀수) : ";
+	cin >> n;
+	if (!is_it_odd(n)) {
+		while (!is_it_odd(n))
+		{
+			cout << "홀수를 입력하세요" << endl;
+			cout << "N의 값(홀수) : ";
+			cin >> n;
+		}
+	}
+
+	num_of_square = n * n; // 마방진 크기
+
+	int** array = new int* [n]; //세로 생성
+	arraymk(array, n); // 가로 생성, 0으로 초기화
+	array = magicmk(array,n); // 마방진 생성
+
+	N_size(n * n);
+	Say(array, n); // 결과 출력
+	delete array;
+	return 0;
+}
+
+bool is_it_odd(int n) {
+	if (n % 2 == 0) {
+		return false;
+	}
+	else
+		return true;
+}
 
 int N_size(int num_of_square) {
 	int si = 10;
@@ -43,17 +79,7 @@ void Say(int** array,int n) {
 	}
 
 }
-
-int main() {
-	int n = 0;
-	int num_of_square = 0;
-	int count = 1;
-	cout << "N의 값(홀수) : ";
-	cin >> n;
-	num_of_square = n*n;
-
-	int** array = new int* [n] ; //세로 생성
-	
+int** arraymk(int** array, int n) {
 	for (int i = 0; i < n; i++) { //가로생성
 		array[i] = new int[n];
 	}
@@ -63,16 +89,18 @@ int main() {
 			array[i][j] = 0;
 		}
 	}
-
-
+	return array;
+}
+int** magicmk(int** array, int n) {
+	int count = 1;
+	int num_of_square = n * n;
 	int strx = (n + 1) / 2 - 1; // x 값
 	int stry = n - 1;           // y 값
-
 	while (num_of_square > 0) { // 마방진 푸는 법
 		if (stry > n - 1) {
 			stry = 0;
 		}
-		if (strx > n - 1 ) {
+		if (strx > n - 1) {
 			strx = 0;
 		}
 		if (array[stry][strx] != 0) {
@@ -84,13 +112,6 @@ int main() {
 		count++;
 		num_of_square--;
 	}
-
-	N_size(n*n);
-	Say(array,n);
-	
-	delete array;
-
-
-
-	return 0;
+	return array;
 }
+
