9b5d6a0b472e42196e1d5c16cd275a056b42e84e
sangwon <psw7528@gamil.com>
2023-09-21 09:38:07 +0900

    project6

--- cpp_posco_project/Project1/project6_caculater.cpp
+++ cpp_posco_project/Project1/project6_caculater.cpp
@@ -9,89 +9,80 @@ using namespace std;
 
 class Cal {
 public:
-	static int answer;
-	virtual int get_answer() { return answer; };
-	int back() { answer = answer_before; };
+	int answer;
+	int get_answer() { return answer; };
 	Cal() {};
 	Cal(int input) { answer = input; };
+	void set_a(int a) {
+		this->a = a;
+	}
+	void add() {
+		answer += a ;
+	}
+	void mul() {
+		answer *= a ;
+	}
+	void mi() {
+		answer -= a ;
+	}
+	void devi() {
+		if ((a != 0)) answer /= a ;
+		else cout << "0으로 나눌 수 없습니다.";
+	}
+	void back() {
+		answer = 0;
+	}
+
 protected:
+	int a;
 	int answer_before;
 
 };
 
-class Plus : public Cal {
-	int get_answer(){
-		return answer;
-	}
-	Plus(int input) {
-		answer += input;
-	}
-};
-
-class multi : public  Cal {
-	int get_answer() {
-		return this->answer;
-	}
-	multi(int input) {
-		answer *= input;
-	}
-};
-
-class minus : public  Cal {
-	int get_answer() {
-		return answer;
-	}
-	minus(int input) {
-		answer *= input;
-	}
-
-};
-
-class divi : public  Cal {
-	int get_answer() {
-		return answer;
-	}
-	divi(int input) {
-		answer *= input;
-	}
-
-};
-
-int Cal::answer = 0;
 
 int main() {
 
-	int input1;
-	int input2;
-	string oper;
-	cout << "숫자를 입력하세요 : ";
+	string oper = "";
+	string selec = "";
+	int input1 = 0;
+	int answer = 0;
+	cout << "첫 번째 값 : ";
 	cin >> input1;
-	Cal cal(input1);
-	cout << "연산자를 입력하세요 : ";
-	cin >> oper;
-	cout << "숫자를 입력하세요 : ";
-	cin >> input2;
-	while (oper != "EXIT") {
+	cout << "두 번째 값 : ";
+	cin >> answer;
+	Cal c(answer);
+	c.set_a(input1);
+
+	while (selec != "EXIT") {
+		cout << "연산자를 입력해주세요 :";
+		cin >> oper;
 		if (oper == "+") {
-			Plus plus();
+			c.add();
 		}
 		else if (oper == "-") {
-
+			c.mi();
 		}
 		else if (oper == "*") {
-
+			c.mul();
 		}
 		else if (oper == "/") {
-
+			c.devi();
 		}
-		else if (oper == "AC") {
-
-		}
-		else if (oper == "Y") {
-
-		}
+		cout << "결과 : " << c.get_answer() << endl;
 		cout << "연산을 계속 진행 하시겠습니까?(Y: 계속, AC: 초기화, EXIT : 종료) : ";
-		cin >> oper;
+		cin >> selec;
+		if (selec == "AC") {
+			c.back();
+			cout << "결과 : " << c.get_answer() << endl;
+			cout << "숫자를 입력해주세요 : ";
+			cin >> input1;
+			c.set_a(input1);
+		}
+		else if (selec == "Y") {
+			cout << "숫자를 입력해주세요 : ";
+			cin >> input1;
+			c.set_a(input1);
+		}
 	}
 
 }
